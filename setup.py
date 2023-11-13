10a1a0782dd72f288596599c56ffe8dd2f0fb1b4
Samir Patel <sampatel@ciena.com>
2023-11-12 20:17:28 -0500

    added long description to reflect it on pypi.org

--- setup.py
+++ setup.py
@@ -1,14 +1,37 @@
 from distutils.core import setup
+
+long_description = """
+    package name: decolyzer
+    description: the package has multiple functions in it as below:
+    
+    decogoat: This is a decorator function which will help to run commands/processes/scripts 
+    (e.g. tcpdump/top etc.) before and after the execution of your own function.
+    The function will delete any existing files before the execution of your own function.
+    The function will also rename the files after the execution of your own function.
+    
+    Ssh class: This is a class based off of paramiko package and will help create ssh connections.
+    
+    scp_operation: This function mimics scp command of linux/unix os.
+    
+    analyze_capture: This is a function to parse traffic capture such as e.g. capture.pcap
+    
+    Help documentation: 
+    https://github.com/patelsamirj111/decolyzer/blob/main/README.md
+    https://github.com/patelsamirj111/decolyzer
+"""
+
 setup(
     name = 'decolyzer',
     packages = ['decolyzer'],
-    version = '1.1',
+    version = '1.2',
     license='MIT',
     description = 'decolyzer package includes functions which can help to enhance your own function implementation',
+    long_description = long_description,
+    long_description_content_type = 'text/x-rst',
     author = 'Samir',
     author_email = 'samir198@gmail.com',
     url = 'https://github.com/patelsamirj111',
-    download_url = 'https://github.com/patelsamirj111/decolyzer/archive/refs/tags/v1.1.tar.gz',
+    download_url = 'https://github.com/patelsamirj111/decolyzer/archive/refs/tags/v1.2.tar.gz',
     keywords = ['decorator', 'python', 'wireshark', 'scp', 'tcpdump', 'paramiko', 'ssh', 'process'],
     install_requires=[
         'paramiko',
